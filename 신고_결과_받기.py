e8b4e34c0652cdef6e79d46e8a8d750bd2e88540
unknown <musicemfdj@konkuk.ac.kr>
2022-09-15 20:33:49 +0900

    doing 신고결과받기

---
+++ 신고_결과_받기.py
@@ -0,0 +1,16 @@
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
+
+
+
+def solution(id_list, report, k):
+	answer = []
+	listed_id = list(id_list)
+	listed_repot = list(report)
+	
+	print("A")
+	return answer
+
+print("AA")
+print(solution(id_list, report, k))
