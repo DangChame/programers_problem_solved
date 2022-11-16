e933533b10222ad0132605206827b40c7da4ef25
unknown <musicemfdj@konkuk.ac.kr>
2022-11-16 19:31:42 +0900

    폰켓몬문제풀이 완료

--- prac_coding_test/Hash/폰켓몬.py
+++ prac_coding_test/Hash/폰켓몬.py
@@ -1,19 +1,36 @@
 def solution(nums):
-    answer = 0
-    return answer
+	answer = 0
 
+	print(nums)
+	nb_of_poket = len(nums)
+	nb_of_poket /= 2
+	nums = set(nums)
+	nb_of_set = len(nums)
+	if nb_of_set >= nb_of_poket:
+		answer = nb_of_poket
+	else:
+		answer = nb_of_set
+	return int(answer)
+
+
+
+
+
+
+# 테스트 부
 nums1 = [3,1,2,3]
 nums2 = [3,3,3,2,2,4]
 nums3 = [3,3,3,2,2,2]
+nums = [nums1,nums2,nums3]
 
-answer = solution(nums1)
 
-result1 = 2
-result2 = 3
-result3 = 2
-
-if (answer == result1):
-	print("correct")
-else:
-	print("wrong")
+result = [2, 3, 2]
+i = 0
+while (i < 3):
+	answer = solution(nums[i])
+	if (answer == result[i]):
+		print("너의 답 : " + str(answer)+" 정답: " + str(result[i])+ "	" + str(i+1)+"번째 correct")
+	else:
+		print("너의 답 : " + str(answer)+" 정답: " + str(result[i])+"	"+ str(i+1)+"번째 wrong")
+	i += 1
 
